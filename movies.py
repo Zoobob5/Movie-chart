1d8c849fb3ebd3e7c5e76036f60e44c75ae101ad
Zoobob5 <bwin.cr7@gmail.com>
2024-08-15 21:19:12 -0700

    adding another movie

--- movies.py
+++ movies.py
@@ -1,5 +1,6 @@
 movies = {
     "Inception": {"genre": "Sci-Fi", "director": "Christopher Nolan", "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]},
     "The Dark Knight": {"genre": "Action", "director": "Christopher Nolan", "actors": ["Christian Bale", "Heath Ledger"]},
-    "Men in Black": {"genre": "Sci-Fi", "director": "Barry Sonnenfeld", "actors": ["Will Smith", "Tommy Lee Jones"]}
+    "Men in Black": {"genre": "Sci-Fi", "director": "Barry Sonnenfeld", "actors": ["Will Smith", "Tommy Lee Jones"]},
+    "Toy Story": {"genre": "Comedy", "director": "John Lasseter", "actors": ["Tom Hanks", "Tim Allen"]}
 }
