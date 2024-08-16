6e59ab0c009e1d08f65d3ce6282ac2f9d31233fc
Zoobob5 <bwin.cr7@gmail.com>
2024-08-15 20:52:56 -0700

    the beginnig with some examples

---
+++ movies.py
@@ -0,0 +1,6 @@
+movies = {
+    "Inception": {"genre": "Sci-Fi", "director": "Christopher Nolan", "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]},
+    "The Dark Knight": {"genre": "Action", "director": "Christopher Nolan", "actors": ["Christian Bale", "Heath Ledger"]},
+    # sample movies
+    
+}
