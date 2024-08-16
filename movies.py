12337dbe5a06362a475992d80f806b43e9c3739c
Zoobob5 <bwin.cr7@gmail.com>
2024-08-15 23:19:56 -0700

    adding imports

--- movies.py
+++ movies.py
@@ -2,5 +2,7 @@ movies = {
     "Inception": {"genre": "Sci-Fi", "director": "Christopher Nolan", "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]},
     "The Dark Knight": {"genre": "Action", "director": "Christopher Nolan", "actors": ["Christian Bale", "Heath Ledger"]},
     "Men in Black": {"genre": "Sci-Fi", "director": "Barry Sonnenfeld", "actors": ["Will Smith", "Tommy Lee Jones"]},
-    "Toy Story": {"genre": "Comedy", "director": "John Lasseter", "actors": ["Tom Hanks", "Tim Allen"]}
+    "Toy Story": {"genre": "Comedy", "director": "John Lasseter", "actors": ["Tom Hanks", "Tim Allen"]},
+    "Pulp Fiction": {"genre": "Crime", "director": "Quentin Tarantino", "actors": ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]
+    }
 }
--- script.py
+++ script.py
@@ -1,2 +1,6 @@
 from math import inf, sqrt
 from heapq import heappop, heappush
+from movies import movies
+
+
+print(movies)
