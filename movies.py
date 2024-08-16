c5241c86e5695b3add35809edd661ed9d0b37847
Zoobob5 <bwin.cr7@gmail.com>
2024-08-15 21:08:52 -0700

    have added more movies

--- movies.py
+++ movies.py
@@ -1,6 +1,5 @@
 movies = {
     "Inception": {"genre": "Sci-Fi", "director": "Christopher Nolan", "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]},
     "The Dark Knight": {"genre": "Action", "director": "Christopher Nolan", "actors": ["Christian Bale", "Heath Ledger"]},
-    # sample movies
-    
+    "Men in Black": {"genre": "Sci-Fi", "director": "Barry Sonnenfeld", "actors": ["Will Smith", "Tommy Lee Jones"]}
 }
