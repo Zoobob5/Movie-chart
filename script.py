c91d937921f6ff6c407b5b934a20ad63e0ed3169
Zoobob5 <bwin.cr7@gmail.com>
2024-08-17 23:31:11 -0500

    added a user input for picked movie and changed Men In Black to title style to work with title

--- script.py
+++ script.py
@@ -7,20 +7,19 @@ from dfs import dfs
 
 
 #print(movies)
+
+def user_in(graph):
+    user = input("Pick a recommended movie title: ").title().strip()
+    if user:
+        title = user.title()
+        print(f"You picked: {title}")
+
+    if title in graph:
+        return title
+    else:
+        print("Movie not found. Please tyhpe in a listed movie.")
+        return user_in(graph)
+
 graph1 = movie_graph(movies)
 print(graph1)
-def user_in(graph):
-    print(graph1)
-    user = input("Pick a recommended movie title: ")
-    if user in graph:
-        recc = bfs(graph, user)
-        if recc:
-            print("Movies similar or related to {}".format(user))
-            for i in recc:
-                print(i)
-
-        else:
-            print("{} is not in the recommended list, please pick a recommended movie".format(user))
-            return user_in()
-
-user_in()
+user_in(graph1)
