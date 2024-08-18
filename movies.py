5c2f314a75ec661b78c53143a9dc859edbe2a8ed
Zoobob5 <bwin.cr7@gmail.com>
2024-08-18 18:18:12 -0500

    added a recursive funtion to repeat ask if the inputs were wrong, or when the function is complete, added another movie. assignment complete

--- movies.py
+++ movies.py
@@ -3,6 +3,6 @@ movies = {
     "The Dark Knight": {"genre": "Action", "director": "Christopher Nolan", "actors": ["Christian Bale", "Heath Ledger"]},
     "Men In Black": {"genre": "Sci-Fi", "director": "Barry Sonnenfeld", "actors": ["Will Smith", "Tommy Lee Jones"]},
     "Toy Story": {"genre": "Comedy", "director": "John Lasseter", "actors": ["Tom Hanks", "Tim Allen"]},
-    "Pulp Fiction": {"genre": "Crime", "director": "Quentin Tarantino", "actors": ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]
-    }
+    "Pulp Fiction": {"genre": "Crime", "director": "Quentin Tarantino", "actors": ["John Travolta", "Uma Thurman", "Samuel L. Jackson"]},
+    "Robin Hood Men In Tights": {"genre": "Comedy", "director": "Mel Brooks", "actors": ["Cary Elwes", "Amy Yasbeck", "Dave Chappelle"]}
 }
--- script.py
+++ script.py
@@ -9,7 +9,7 @@ from dfs import dfs
 #print(movies)
 
 def user_in(graph):
-    user = input("Pick a recommended movie title: ").title().strip()
+    user = input("Pick a recommended full movie title: ").title().strip()
     if user:
         title = user.title()
         print(f"You picked: {title}")
@@ -20,6 +20,52 @@ def user_in(graph):
         print("Movie not found. Please tyhpe in a listed movie.")
         return user_in(graph)
 
-graph1 = movie_graph(movies)
-print(graph1)
-user_in(graph1)
+def valid_search():
+    search = input("Choose search style, BFS (Breadth) or DFS (Depth): ").upper()
+    if search == "BFS" or search == "DFS":
+        return search
+    else:
+        print("Please pick a valid search option.")
+        return valid_search()
+
+def again():
+    ask = input("Would you like to search for another movie recomendation? Please pick Yes or No: ").title().strip()
+
+    if ask == "Yes":
+        return get_rec()
+    elif ask == "No":
+        print("Thank you, have a good day.")
+    else:
+        print("Please pick a valid option.")
+        return again()
+
+def get_rec():
+    #the graph
+    graph1 = movie_graph(movies)
+    #show the graph
+    print(graph1)
+    #get movie from person
+    user = user_in(graph1)
+    #pick search style
+    search = valid_search()
+
+    if search == "BFS":
+        rec = bfs(graph1, user)
+    elif search == "DFS":
+        rec = dfs(graph1, user)
+
+
+    if rec:
+        print("{} is related to: ".format(user))
+        if len(rec) <= 1:
+            print(f"There are no other movies related to {rec[0]}")
+
+        for i in rec:
+            print(i)
+    again()
+
+
+
+
+#BEGIN
+get_rec()
